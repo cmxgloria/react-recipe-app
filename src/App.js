0cc916febaae7f623890afeca772fe87dde2cd3c
Gloria Chen <cmxgloria@gmail.com>
2019-05-29 14:39:19 +1000

    import Form to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Form from './components/Form';
 class App extends Component {
   render() {
     return (
@@ -8,6 +8,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
+        <Form />
       </div>
     );
   }
