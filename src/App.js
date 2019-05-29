c70ea16277906f733430967df336a32513370db0
Gloria Chen <cmxgloria@gmail.com>
2019-05-29 19:00:39 +1000

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import Form from './components/Form';
+const API_KEY = '92f74e2d208972d83294d191e27b85b3';
+
+
 class App extends Component {
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=5`)
+    console.log(recipeName);
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
-        <Form />
+        <Form getRecipe={this.getRecipe} />
       </div>
     );
   }
