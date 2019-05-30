6742301180f30f784af9d50db994e1806a67ba6d
Gloria Chen <cmxgloria@gmail.com>
2019-05-30 21:42:29 +1000

    this.state.activeRecipe.length

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -17,15 +17,21 @@ class Recipe extends React.Component {
     console.log(this.props);
     return (
       < div className="container">
-        <div className="active-recipe">
-          <img className="active-recipe__img" src={recipe.image_url} alt={recipe.title} />
-          <h3 className="active-recipe__title">{recipe.title}</h3>
-          <h4 className="active-recipe__publisher">
-            Publisher:<span>{recipe.publisher}</span>
-          </h4>
-          <p className="active-recipe__website">
-            Website:<span><a href={recipe.publisher_url}>{recipe.publisher_url}</a></span></p>
-        </div>
+        {
+          this.state.activeRecipe.length !== 0 &&
+          <div className="active-recipe">
+            <img className="active-recipe__img" src={recipe.image_url} alt={recipe.title} />
+            <h3 className="active-recipe__title">{recipe.title}</h3>
+            <h4 className="active-recipe__publisher">
+              Publisher:<span>{recipe.publisher}</span>
+            </h4>
+            <p className="active-recipe__website">
+              Website:<span><a href={recipe.publisher_url}>{recipe.publisher_url}</a>
+              </span>
+            </p>
+            <button className="active-recipe__button">Go Home</button>
+          </div>
+        }
       </div >
     );
   }
