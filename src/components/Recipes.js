33e4c5ab516b4e886d19189d1bd575a43f093ce3
gloria <cmxgloria@gmail.com>
2020-09-02 14:26:20 +1000

    deploy

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,30 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 const Recipes = props => (
   <div className="container">
     <div className="row">
-      {props.recipes.map((recipe) => {
-        return (<div key={recipe.title} className="col-md-4" style={{ marginBottom: "2rem" }}>
-          <div className="recipe__box-img">
-            <img src={recipe.image_url} alt={recipe.title} />
-            <div className="recipe__text">
-              <h5 className="recipe__title">
-                {recipe.title.length < 20 ? `${recipe.title}` : `${recipe.title.substring(0, 25)}...`}
-              </h5>
-              <p className="recipes__subtitle">Publisher:<span>{recipe.publisher}
-              </span>
-              </p>
+      {props.recipes.map(recipe => {
+        return (
+          <div
+            key={recipe.recipe.label}
+            className="col-md-4"
+            style={{ marginBottom: "2rem" }}
+          >
+            <div className="recipe__box-img">
+              <img src={recipe.recipe.image} alt={recipe.recipe.label} />
+              <div className="recipe__text">
+                <p className="recipes__calories">
+                  Calories:<span>{recipe.recipe.calories}</span>
+                </p>
+                <p>
+                  Ingredients:<span>{recipe.recipe.ingredients}</span>
+                </p>
+              </div>
+              <button className="recipe_button">
+                <Link
+                  to={{
+                    pathname: `./recipe/${recipe.recipe.label}`,
+                    state: { recipe: recipe.recipe.label }
+                  }}
+                >
+                  View Recipe
+                </Link>
+              </button>
             </div>
-            <button className="recipe_button">
-              <Link to={{
-                pathname: `./recipe/${recipe.recipe_id}`,
-                state: { recipe: recipe.title }
-              }}>
-                View Recipe
-              </Link>
-            </button>
           </div>
-        </div>
         );
       })}
     </div>
